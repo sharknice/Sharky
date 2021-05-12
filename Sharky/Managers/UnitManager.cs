9ad209f1e4dcf3d5b721e73ab0bc26429324d597
sharknice <glowystarman@yahoo.com>
2021-05-12 01:51:49 -0500

    fix drone mining

--- Sharky/Managers/UnitManager.cs
+++ Sharky/Managers/UnitManager.cs
@@ -46,7 +46,7 @@ namespace Sharky.Managers
 
             ActiveUnitData.DeadUnits = new List<ulong>();
 
-            UndeadTypes = new List<UnitTypes> { UnitTypes.ZERG_BROODLING, UnitTypes.ZERG_EGG, UnitTypes.ZERG_LARVA, UnitTypes.TERRAN_KD8CHARGE, UnitTypes.ZERG_OVERLORD, UnitTypes.ZERG_OVERLORDCOCOON, UnitTypes.ZERG_OVERLORDTRANSPORT, UnitTypes.ZERG_TRANSPORTOVERLORDCOCOON, UnitTypes.ZERG_DRONE };
+            UndeadTypes = new List<UnitTypes> { UnitTypes.ZERG_BROODLING, UnitTypes.ZERG_EGG, UnitTypes.ZERG_LARVA, UnitTypes.TERRAN_KD8CHARGE, UnitTypes.ZERG_OVERLORD, UnitTypes.ZERG_OVERLORDCOCOON, UnitTypes.ZERG_OVERLORDTRANSPORT, UnitTypes.ZERG_TRANSPORTOVERLORDCOCOON };
         }
 
         public override bool NeverSkip { get { return true; } }
