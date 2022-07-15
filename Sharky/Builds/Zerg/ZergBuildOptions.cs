7e63b84dca76bb7848e50f6e9b3012eb6f3a064a
SATIK-PC\satik <satan.lord@seznam.cz>
2022-07-15 10:25:33 +0200

    Added zergbuildoption for deciding what second queen should do as first action (creep / inject)

--- Sharky/Builds/Zerg/ZergBuildOptions.cs
+++ Sharky/Builds/Zerg/ZergBuildOptions.cs
@@ -6,9 +6,15 @@
         /// If true, creep spread priority is to connect main with natural.
         /// </summary>
         public bool ConnectMainWithNatWithCreep { get; set; } = false;
+        
         /// <summary>
         /// How many first tumors have increased score for forward tumors.
         /// </summary>
         public int TumorsPreferForward { get; set; } = 3;
+
+        /// <summary>
+        /// If true, second born queen first spawns creep tumor at natural, this speeds up the creep spread.
+        /// </summary>
+        public bool SecondQueenPreferCreepOnBorn { get; set; } = true;
     }
 }
