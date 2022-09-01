c96239decbb60d472c123b55fbc8a62e658ca805
SATIK-PC\satik <satan.lord@seznam.cz>
2022-09-01 09:27:16 +0200

    Added mass raven enemy strategy, improved equivalentenemy units counting

---
+++ Sharky/EnemyStrategies/Terran/MassRavens.cs
@@ -0,0 +1,21 @@
+﻿using Sharky.DefaultBot;
+
+namespace Sharky.EnemyStrategies.Terran
+{
+    public class MassRavens : EnemyStrategy
+    {
+        public MassRavens(DefaultSharkyBot defaultSharkyBot) : base(defaultSharkyBot) { }
+
+        protected override bool Detect(int frame)
+        {
+            if (EnemyData.EnemyRace != SC2APIProtocol.Race.Terran) { return false; }
+
+            if (UnitCountService.EquivalentEnemyTypeCount(UnitTypes.TERRAN_RAVEN) >= 6)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
