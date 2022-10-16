f12350d069100a144a3ac0ae2c283f718b53fb26
sharknice <glowystarman@yahoo.com>
2022-10-16 14:53:34 -0500

    fix for banelings not attacking

--- Sharky/UnitDataService.cs
+++ Sharky/UnitDataService.cs
@@ -148,7 +148,7 @@ namespace Sharky
             if (unitType == UnitTypes.TERRAN_LIBERATORAG || unitType == UnitTypes.PROTOSS_DISRUPTOR || unitType == UnitTypes.PROTOSS_ORACLE
                 || unitType == UnitTypes.PROTOSS_CARRIER || unitType == UnitTypes.TERRAN_WIDOWMINE || unitType == UnitTypes.TERRAN_WIDOWMINEBURROWED
                 || unitType == UnitTypes.TERRAN_CYCLONE || unitType == UnitTypes.ZERG_INFESTOR || unitType == UnitTypes.TERRAN_BATTLECRUISER
-                || unitType == UnitTypes.TERRAN_BUNKER || unitType == UnitTypes.PROTOSS_SENTRY || unitType == UnitTypes.PROTOSS_VOIDRAY || unitType == UnitTypes.PROTOSS_ADEPTPHASESHIFT)
+                || unitType == UnitTypes.TERRAN_BUNKER || unitType == UnitTypes.PROTOSS_SENTRY || unitType == UnitTypes.PROTOSS_VOIDRAY || unitType == UnitTypes.PROTOSS_ADEPTPHASESHIFT || unitType == UnitTypes.ZERG_BANELING || unitType == UnitTypes.ZERG_BANELINGBURROWED)
             {
                 return true;
             }
