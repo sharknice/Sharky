0e340be46282b5d63b6706153dce4631b96a1fc9
satik <satan.lord@seznam.cz>
2023-07-29 01:47:02 +0200

    Added ProducerSelector which helps to determine which unit to use to build other unit, Updated logic for detecting ES invisible attacks Some refactors in UnitBuilder

---
+++ Sharky/Builds/ProductionSelectors/ZergProducerSelector.cs
@@ -0,0 +1,57 @@
+﻿namespace Sharky.Builds.ProductionSelectors
+{
+    public class ZergProducerSelector : IProducerSelector
+    {
+        private EnemyData EnemyData;
+        private ActiveUnitData ActiveUnitData;
+        private SharkyUnitData SharkyUnitData;
+        private BaseData BaseData;
+        private TargetingData TargetingData;
+
+        public ZergProducerSelector(DefaultSharkyBot sharkyBot)
+        {
+            EnemyData = sharkyBot.EnemyData;
+            ActiveUnitData = sharkyBot.ActiveUnitData;
+            SharkyUnitData = sharkyBot.SharkyUnitData;
+            BaseData = sharkyBot.BaseData;
+            TargetingData = sharkyBot.TargetingData;
+        }
+
+        public UnitCommander SelectBestProducer(UnitTypes unitType, IEnumerable<UnitCommander> producers)
+        {
+            if (unitType == UnitTypes.ZERG_OVERSEER)
+            {
+                return SelectBestOverseerProducer(producers);
+            }
+
+            return producers.First();
+        }
+
+        private UnitCommander SelectBestOverseerProducer(IEnumerable<UnitCommander> producers)
+        {
+            Vector2 desiredMorphPoint;
+            if (EnemyData.EnemyStrategies[nameof(InvisibleAttacks)].Active || EnemyData.EnemyStrategies[nameof(InvisibleAttacksSuspected)].Active)
+            {
+                var enemyInvisibleUnits = ActiveUnitData.EnemyUnits.Where(x => InvisibleAttacks.IsNonObserverCloakableUnit(x.Value)).Select(x => x.Value);
+
+                if (enemyInvisibleUnits.Any())
+                {
+                    // Morph near invisible units
+                    desiredMorphPoint = TargetingData.NaturalBasePoint.ToVector2();
+                }
+                else
+                {
+                    // Morph at home to be as safe as possible against invis
+                    desiredMorphPoint = TargetingData.NaturalBasePoint.ToVector2();
+                }
+            }
+            else
+            {
+                // Morph near enemy base for scouting
+                desiredMorphPoint = TargetingData.NaturalFrontScoutPoint.ToVector2();
+            }
+
+            return producers.OrderBy(x => x.UnitCalculation.Position.DistanceSquared(desiredMorphPoint)).OrderBy(x => x.UnitCalculation.EnemiesThreateningDamage.Count).First();
+        }
+    }
+}
